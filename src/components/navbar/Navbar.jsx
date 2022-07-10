7e534142a32297675b544640adf55aae32685b11
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-10 07:29:39 -1200

    Install tailwind css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="container mx-auto bg-yellow-600">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+      aliquam perferendis illo incidunt iste voluptatibus non tempore, voluptate
+      quam molestias fugit alias porro voluptas eligendi aut nihil reprehenderit
+      laboriosam quaerat.
+    </nav>
+  );
 };
 
 export default Navbar;
