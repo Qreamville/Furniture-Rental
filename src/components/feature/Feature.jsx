88c8832dcba01cf05b51e1941d37ea5b6183a850
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-11 22:55:38 -1200

    Modify speling mistake

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -6,9 +6,11 @@ const Feature = () => {
     <section>
       <div className="flex flex-col-reverse items-center justify-center md:flex-row">
         <div className="flex flex-wrap flex-col items-start justify-center gap-6 px-6">
-          <div class="flex flex-wrap flex-col items-start gap-2">
-            <h2 class="text-4xl font-bold capitalize">Relax, You're home!</h2>
-            <p class="max-w-md">
+          <div className="flex flex-wrap flex-col items-start gap-2">
+            <h2 className="text-4xl font-bold capitalize">
+              Relax, You're home!
+            </h2>
+            <p className="max-w-md">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
               dolore voluptatem dolores optio sed ut rem, libero expedita quam
               ex.
@@ -19,10 +21,10 @@ const Feature = () => {
             </p>
           </div>
           <a
-            class="flex py-2 px-6  gap-2 shadow-xl bg-yellow-600 rounded-md font-bold capitalize hover:bg-red-600 hover:text-white text-1xl"
+            className="flex py-2 px-6  gap-2 shadow-xl bg-yellow-600 rounded-md font-bold capitalize hover:bg-red-600 hover:text-white text-1xl"
             href="/"
           >
-            <span class="flex">Sign Up</span>
+            <span className="flex">Sign Up</span>
           </a>
         </div>
         <div>
